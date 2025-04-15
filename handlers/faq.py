f6c6bebc6f17b8c2f4fc3b71541fb4a861fe4a51
a1113361 <a1113361@mail.nuk.edu.tw>
2025-04-16 01:00:45 +0800

    fix: 更新 default 回覆內容

--- handlers/faq.py
+++ handlers/faq.py
@@ -2,7 +2,6 @@ from linebot.models import TextSendMessage, FlexSendMessage
 
 def handle(msg):
     msg = msg.strip()
-    print(f"👉 [faq.handle] 收到訊息：{repr(msg)}")
 
     if msg in ["常見問題"]:
         flex_content = {
@@ -59,16 +58,16 @@ def handle(msg):
         }
         return FlexSendMessage(alt_text="常見問題", contents=flex_content)
 
-    elif "什麼是食品添加物" in msg:
+    elif msg == "我想知道「什麼是食品添加物」":
         return TextSendMessage(text="食品添加物是指為了改善食品的色、香、味、口感、保存期限等，而額外加入食品中的物質，例如防腐劑、色素、甜味劑、抗氧化劑等。")
 
-    elif "常見的食品添加物" in msg:
+    elif msg == "我想知道「常見的食品添加物」":
         return TextSendMessage(text="防腐劑：防止食品腐爛變質，防止微生物的生長，有效延長保存期限；抗氧化劑：抗氧化劑防止食品中的油脂氧化；著色劑（色素）：用來提升食品的外觀，改善加工過程中的食物褪色，使其更具吸引力；調味劑：調味劑用來增強食品的風味，調整食物的味道，使其更加美味。")
 
-    elif "對食品安全有疑慮怎麼辦" in msg:
+    elif msg == "我想知道「對食品安全有疑慮怎麼辦」":
         return TextSendMessage(text="如果您有食品檢舉、食品諮詢、消費問題、中小企業諮詢、生鮮農產諮詢等食品安全相關問題，以市話或手機直撥「1919」，可得到即時服務與處理。")
 
-    elif "什麼是無添加驗證/潔淨標章" in msg:
+    elif msg == "我想知道「什麼是無添加驗證/潔淨標章」":
         return TextSendMessage(text="藉由A.A.(無添加協會)進行檢核機制，透過第三方審查與評核，合格後所獲得之標章，以六大類食品添加物為驗證標準，無含有鮮味劑、漂白劑、品質改良劑、人工色素、人工香精及防腐劑；證書有效期為一年，期間包含不定期追蹤查核，若不符合審查標準，證書即廢止。自廢止日起一年後，始得再提出申請。")
 
     return None
