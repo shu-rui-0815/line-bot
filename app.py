5ecf4691c1c78ee49332cff177c85148365cf9f4
a1113361 <a1113361@mail.nuk.edu.tw>
2025-04-16 00:54:41 +0800

    fix: 什麼是食品添加物

--- app.py
+++ app.py
@@ -24,19 +24,20 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    print(f"收到訊息：{repr(msg)}")
 
     reply = faq.handle(msg)
-
-    if reply is None:
-        reply = news.handle(msg)
-
-    if reply is None:
-        reply = default.fallback(msg)
-
-    if reply is not None:
-        line_bot_api.reply_message(event.reply_token, reply)
+    if reply:
+        print("FAQ 命中")
     else:
-        print("沒有任何模組回應")
+        reply = news.handle(msg)
+        if reply:
+            print("NEWS 命中")
+        else:
+            print("沒命中，走 fallback")
+            reply = default.fallback(msg)
+
+    line_bot_api.reply_message(event.reply_token, reply)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
