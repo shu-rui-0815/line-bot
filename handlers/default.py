99350d0cd9da89e8e0c51c530a3e546b543426f9
a1113361 <a1113361@mail.nuk.edu.tw>
2025-04-16 01:02:22 +0800

    fix: 同步本地 default.py 內容

--- handlers/default.py
+++ handlers/default.py
@@ -2,5 +2,5 @@ from linebot.models import TextSendMessage
 
 def fallback(msg):
     return TextSendMessage(
-        text="還不太懂你的意思...\n請點選選單中的圖片，或輸入關鍵字來讓我更好理解你的需求！"
+        text="我還不太懂你的意思...\n請點選選單中的圖片，或輸入關鍵字來讓我更好理解你的需求！"
     )
